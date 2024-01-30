a249f7ec5d5499c4e954f499735ba79faa1fa198
sreejaya <sreejayam16@gmail.com>
2024-01-30 15:40:13 +0530

    connected to sql

--- new/actions/actions.py
+++ new/actions/actions.py
@@ -1,78 +1,95 @@
-# # This files contains your custom actions which can be used to run
-# # custom Python code.
-# #
-# # See this guide on how to implement these action:
-# # https://rasa.com/docs/rasa/custom-actions
+# This files contains your custom actions which can be used to run
+# custom Python code.
 #
-#
-# # This is a simple example for a custom action which utters "Hello World!"
-#
-# # from typing import Any, Text, Dict, List
-# #
-# # from rasa_sdk import Action, Tracker
-# # from rasa_sdk.executor import CollectingDispatcher
-# #
-# #
-# # class ActionHelloWorld(Action):
-# #
-# #     def name(self) -> Text:
-# #         return "action_hello_world"
-# #
-# #     def run(self, dispatcher: CollectingDispatcher,
-# #             tracker: Tracker,
-# #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-# #
-# #         dispatcher.utter_message(text="Hello World!")
-# #
-# #         return []
-#
-#
-#
-# # File: actions.py
-#
-# from typing import Any, Text, Dict, List
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-# import mysql.connector
-#
-# class ActionDataUpdate(Action):
-#     def name(self) -> Text:
-#         return "action_data_update"
-#
-#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#         # Retrieve user inputs from the conversation
-#         name = tracker.get_slot("name")
-#         crop_stage = tracker.get_slot("Crop_stage")
-#
-#         # Connect to the MySQL database
-#         db = mysql.connector.connect(
-#             host="127.0.0.1",
-#             user="root",
-#             password="123Sreejaya123",
-#             database="db1",
-#         )
-#
-#         # Create a cursor object
-#         cursor = db.cursor()
-#
-#         # Create the "simplify" table if it doesn't exist
-#         cursor.execute("CREATE TABLE IF NOT EXISTS simplify (name VARCHAR(255), Crop_stage VARCHAR(255));")
-#
-#         # Build and execute the SQL query to insert data
-#         query = 'INSERT INTO simplify(name, crop_stage) VALUES("{0}", "{1}");'.format(name, crop_stage)
-#         cursor.execute(query)
-#
-#         # Commit the changes to the database
-#         db.commit()
-#
-#         # Display the number of records inserted
-#         print(cursor.rowcount, "record inserted")
-#
-#         # Close the cursor and database connection
-#         cursor.close()
-#         db.close()
-#
-#         # Respond to the user
-#         dispatcher.utter_message(text=f"Thank you, {name}! Your crop stage ({crop_stage}) has been updated in the database.")
-#
-#         return []
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import mysql.connector as mc
+
+
+class ActionSaveData(Action):
+
+    def name(self) -> Text:
+        return "action_save_data"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        name=tracker.get_slot("name")
+        email=tracker.get_slot("email")
+        try:
+            db=mc.connect(
+                host="127.0.0.1",
+                user="root",
+                password="123Sreejaya123",
+                database='rasa',
+            )
+            cur=db.cursor()
+            insert_query="INSERT INTO users(name,email) VALUES(%s,%s)"
+            data=(name,email)
+            cur.execute(insert_query,data)
+            db.commit()
+            db.close()
+        except Exception as e:
+            print("Error : ", e)
+
+
+
+
+
+        dispatcher.utter_message(response="utter_thanks")
+
+        return []
+
+
+
+class ActionShowAllData(Action):
+
+    def name(self) -> Text:
+        return "action_show_all_data"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        name=tracker.get_slot("name")
+        email=tracker.get_slot("email")
+        try:
+            db=mc.connect(
+                host="127.0.0.1",
+                user="root",
+                password="123Sreejaya123",
+                database='rasa',
+            )
+            cur=db.cursor()
+            select_query="SELECT name,email FROM users"
+            cur.execute(select_query)
+            data=cur.fetchall()
+            db.close()
+            if data:
+                entries="\n".join([f"(name:{row[0]},email{row[1]})" for row in data])
+                response=f"Sure, these are the details of the database:\n{entries}"
+            else:
+                response="The database is empty"
+            dispatcher.utter_message(text=response)
+
+
+        except Exception as e:
+            print("Error : ", e)
+
+            dispatcher.utter_message(text="Sorry,Something went wrong")
+
+
+
+
+
+
+        dispatcher.utter_message(response="utter_thanks")
+
+        return []
